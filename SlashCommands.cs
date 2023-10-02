fe7e5171fd823f50c06948f9717a4b4691ba88c7
felix920506 <felix920506@gmail.com>
2023-10-02 19:27:53 -0400

    Add clear command and clear debug artifacts

--- SlashCommands.cs
+++ SlashCommands.cs
@@ -106,6 +106,7 @@ namespace ReactionRolesBotCS {
     
         //
         // Remove rero command
+
         [SlashCommand("remove", "Removes specified reaction role from message")]
         public async Task removeReroCommand(InteractionContext ctx,
         [Option("message", "Message ID of the target message")] string message,
@@ -126,22 +127,53 @@ namespace ReactionRolesBotCS {
             //
             // Write changes to database
 
-            try {
-                using (var connection = new SqliteConnection("Data Source=database.db")) {
-                    connection.Open();
-                    var command = connection.CreateCommand();
-                    command.CommandText = @"DELETE FROM reactionRoles WHERE message = $message AND emoji = $emoji;";
-                    command.Parameters.AddWithValue("$message", ulong.Parse(message));
-                    command.Parameters.AddWithValue("$emoji", emoji);
-                    command.ExecuteNonQuery();
-                }
-            }
-            catch (Exception ex) {
-                Console.WriteLine(ex.ToString());
+            using (var connection = new SqliteConnection("Data Source=database.db")) {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = @"DELETE FROM reactionRoles WHERE message = $message AND emoji = $emoji;";
+                command.Parameters.AddWithValue("$message", ulong.Parse(message));
+                command.Parameters.AddWithValue("$emoji", emoji);
+                command.ExecuteNonQuery();
             }
 
             DiscordInteractionResponseBuilder responseBuilder2 = new DiscordInteractionResponseBuilder();
             responseBuilder2.WithContent("Reaction role removed (if it existed). Reactions will have to be removed manually due to technical limitations.");
+            responseBuilder2.AsEphemeral();
+            await ctx.CreateResponseAsync(responseBuilder2);
+        }
+
+        //
+        // Clear rero command
+
+        [SlashCommand("clear", "Removes all reaction roles from message")]
+        public async Task clearReroCommand(InteractionContext ctx,
+        [Option("message", "Message ID of the target message")] string message) {
+
+            //
+            // Validate Permissions
+
+            if (!ctx.Member.Permissions.HasPermission(Permissions.ManageRoles)) {
+
+                DiscordInteractionResponseBuilder responseBuilder1 = new DiscordInteractionResponseBuilder();
+                responseBuilder1.WithContent("You need manage roles permissions to use this command");
+                responseBuilder1.AsEphemeral();
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, responseBuilder1);
+                return;
+            }
+
+            //
+            // Write changes to database
+
+            using (var connection = new SqliteConnection("Data Source=database.db")) {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = @"DELETE FROM reactionRoles WHERE message = $message";
+                command.Parameters.AddWithValue("$message", ulong.Parse(message));
+                command.ExecuteNonQuery();
+            }
+
+            DiscordInteractionResponseBuilder responseBuilder2 = new DiscordInteractionResponseBuilder();
+            responseBuilder2.WithContent("All existing reaction roles cleared from message. Reactions will have to be removed manually due to technical limitations.");
             responseBuilder2.AsEphemeral();
             await ctx.CreateResponseAsync(responseBuilder2);
         }
